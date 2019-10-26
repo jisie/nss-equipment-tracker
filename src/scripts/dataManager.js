e712d53de5edd60d68cc601e1bf060453bee1cd3
Allie <allison.tpttn@gmail.com>
2019-10-26 15:00:57 -0500

    adding a post method

--- src/scripts/dataManager.js
+++ src/scripts/dataManager.js
@@ -7,6 +7,16 @@ const API = {
         return fetch("http://localhost:3000/checkoutlog")
             .then(response => response.json())
     },
+    submit(newLog) {
+        return fetch("http://localhost:3000/checkoutlog", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newLog)
+        })
+            .then(response => response.json()) //the object that was just created
+    },
 
 }
 export default API
