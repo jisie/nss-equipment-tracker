dff860bc438a6ec908a0c4d0d183a9a32713e013
Allie <allison.tpttn@gmail.com>
2019-10-26 11:28:26 -0500

    working on rendering types dynamically

---
+++ src/scripts/dataManager.js
@@ -0,0 +1,12 @@
+const API = {
+    typeFetch() {
+        return fetch("http://localhost:3000/types")
+        .then(response => response.json())
+    },
+    logFetch() {
+        return fetch("http://localhost:3000/checkoutlog")
+        .then(response => response.json())
+    },
+    
+}
+export default API
---
+++ src/scripts/main.js
@@ -0,0 +1,18 @@
+import API from "./dataManager.js"
+
+API.typeFetch().then(types => {
+    renderTypes(types);
+});
+
+const renderTypes = (types) => {
+    types.forEach(type => {
+        createTypeListItem(type);
+    });
+};
+
+const createTypeListItem = type => {
+    var node = document.createElement("LI");                 // Create a <li> node
+    var textnode = document.createTextNode(type.Name);         // Create a text node
+    node.appendChild(textnode);                              // Append the text to <li>
+    document.getElementById("typeDropDown").appendChild(node);
+};
