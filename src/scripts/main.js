5ca2e9bff1974cd5e5057ca56517cea440b00a5f
Bito Mann <bito@bitomann.com>
2019-10-26 11:23:16 -0500

    placed nav bar on the DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,12 @@
 const equipFormHtml = (formInfo) => {
     const equipForm = `
+
+        <ul>
+            <li><a href="default.asp">Home</a></li>
+            <li><a href="news.asp">News</a></li>
+            <li><a href="contact.asp">Contact</a></li>
+            <li><a href="about.asp">About</a></li>
+        </ul>
         <form>
             <fieldset>
                 <label for="checkOutDate">Date</label>
